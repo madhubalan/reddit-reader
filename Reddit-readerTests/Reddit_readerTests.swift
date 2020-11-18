e672c49f354db470cd309b00dd1544ed70237f24
Madhubalan <64473905+madhubalan-krishnamoorthy@users.noreply.github.com>
2020-11-18 15:50:41 +0530

    test6

--- Reddit-readerTests/Reddit_readerTests.swift
+++ Reddit-readerTests/Reddit_readerTests.swift
@@ -29,6 +29,7 @@ class Reddit_readerTests: XCTestCase {
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
+            print("test")
             // Put the code you want to measure the time of here.
         }
     }
